bec6a54d4ea1759f9c727247ffef41c3f054e56e
Dan Phung <leftbyte@gmail.com>
2014-02-01 14:32:28 -0800

    Added CRUD stubs (nodepad tutorial step 3).  The expresso testing framework isn't working so I'm abandoning that for now.

--- app.js
+++ app.js
@@ -8,8 +8,11 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
-
+var mongoose = require('mongoose');
 var app = express();
+var db;
+var Document;
+var server;
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -19,25 +22,91 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(express.json());
+app.use(express.multipart());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+    app.use(express.logger({ format: ':method :uri' }));
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+    db = mongoose.connect('mongodb://localhost/nodepad-development');
 };
 
+if ('production' == app.get('env')) {
+    app.use(express.logger());
+    app.use(express.errorHandler());
+    db = mongoose.connect('mongodb://localhost/nodepad-production');
+};
+
+if ('test' == app.get('env')) {
+    app.use(express.logger({ format: ':method :uri' }));
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+    db = mongoose.connect('mongodb://localhost/nodepad-test');
+};
+
+app.Document = Document = require('./models.js').Document(db);
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+// GET
+app.get('/', function(req, res) {
+  res.render('index.jade', {
+    locals: {
+        title: 'Express'
+    }
+  });
 });
 
-mongoose = require('mongoose')
-db = mongoose.connect('mongodb://localhost/nodepad')
+// LIST
+app.get('/documents.:format', function(req, res) {
+    Document.find().all(function(documents) {
+        switch (req.params.format) {
+            case 'json':
+            // 'documents' will contain all of the documents returned by the query
+            res.send(documents.map(function (d) {
+                // return a useful representation of the object that res.send can send as JSON
+                return d.__doc;
+            }));
+            break;
+            default:
+             res.render('documents/index.jade');
+        }
+    });
+});
 
-Document = require('./models.js').Document(db);
+// CREATE
+app.post('/documents.:format?', function(req, res) {
+    var document = new Document(req.body['document']);
+    document.save(function() {
+        switch(req.params.format) {
+            case 'json':
+            res.send(document.__doc);
+            break;
 
-module.exports = app;
+            default:
+            res.redirect('/documents');
+        }
+    });
+});
+
+// READ
+app.get('/documents/:id.:format?', function(req, res) {
+});
+
+// UPDATE
+app.put('/documents/:id.:format?', function(req, res) {
+});
+
+// DELETE
+app.del('/documents/:id.:format?', function(req, res) {
+});
+
+if (!module.parent) {
+    server = http.createServer(app).listen(app.get('port'), function(){
+        console.log('Express server listening on port ' + app.get('port'));
+    });
+}
+
+module.exports = server;
